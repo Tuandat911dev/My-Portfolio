dd69f7ed4f55d98c518b6c057a976763f0cfef85
PhamTuanDat <pdat100111@gmail.com>
2025-12-27 11:03:46 +0700

    use swipper for skill slider

--- src/components/sections/skill/index.tsx
+++ src/components/sections/skill/index.tsx
@@ -1,16 +1,14 @@
 import { SKILLS_DATA } from "helpers/data";
 import { skillsImage } from "helpers/skill.image";
-import Marquee from "react-fast-marquee";
 import "./skill.scss";
 import { Col, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
-import { useContext } from "react";
-import { AppContext } from "@/components/context/app.context";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode } from "swiper/modules";
+import "swiper/css";
 
 const Skill = () => {
   const { t } = useTranslation();
-  const { theme } = useContext(AppContext)!;
   return (
     <Row className="skills-container">
       <Col xs={12} className="my-3 my-md-5">
@@ -19,25 +17,49 @@ const Skill = () => {
         </div>
       </Col>
       <Col xs={12} className="skills-marquee">
-        <Marquee
-          gradient={true}
-          gradientColor={theme === "dark" ? "#0d1224" : "#fff"}
-          speed={50}
-          pauseOnHover={true}
-          pauseOnClick={true}
-          delay={0}
-          play={true}
-          direction="left"
+        <Swiper
+          slidesPerView={7}
+          spaceBetween={5}
+          loop={true}
+          freeMode={true}
+          speed={2000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            300: {
+              slidesPerView: 2,
+            },
+            400: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            800: {
+              slidesPerView: 5,
+            },
+            1100: {
+              slidesPerView: 6,
+            },
+            1170: {
+              slidesPerView: 7,
+            },
+          }}
+          modules={[FreeMode, Autoplay]}
         >
           {SKILLS_DATA.map((skill, id) => (
-            <div className="skill-item" key={id}>
-              <div className="skill-card">
-                <img src={skillsImage(skill)} alt={skill} width={40} height={40} />
-                <p className="skill-name">{skill}</p>
+            <SwiperSlide>
+              <div className="skill-item" key={id}>
+                <div className="skill-card">
+                  <img src={skillsImage(skill)} alt={skill} width={40} height={40} />
+                  <p className="skill-name">{skill}</p>
+                </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </Marquee>
+        </Swiper>
       </Col>
     </Row>
   );
